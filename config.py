fc172e226ff9adb400e8b3a4fdb105a408f9ea74
snlucas <lucas.lambda.101@gmail.com>
2021-07-12 14:24:40 -0300

    #bug-500 -- change secret key on the fly

--- config.py
+++ config.py
@@ -4,12 +4,16 @@ from dotenv import load_dotenv
 
 
 # Use .env file to lock SECRET KEY
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+#basedir = path.abspath(path.dirname(__file__))
+#load_dotenv(path.join(basedir, '.env'))
+
+# Use secrets to handle SECRET KEY
+import secrets
 
 class Config:
     """Base config"""
-    SECRET_KEY = environ.get('SECRET_KEY')
+    #SECRET_KEY = environ.get('SECRET_KEY')
+    SECRET_KEY = secrets.token_urlsafe(32)
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
 
